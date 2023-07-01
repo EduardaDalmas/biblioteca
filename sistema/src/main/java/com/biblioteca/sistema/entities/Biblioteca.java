fe5a39dfc07224019de503f2fde0bb3a16f3352b
Eduarda Brisch Dalmás <du.dalmas@hotmail.com>
2023-06-30 21:11:48 -0300

    validação de formulário

--- sistema/src/main/java/com/biblioteca/sistema/entities/Biblioteca.java
+++ sistema/src/main/java/com/biblioteca/sistema/entities/Biblioteca.java
@@ -1,10 +1,13 @@
 package com.biblioteca.sistema.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 @Table(name= "biblioteca")
@@ -13,6 +16,9 @@ public class Biblioteca {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "nome")
+    @NotBlank(message = "O nome é obrigatório!")
     private String nome;
     private String autor;
     private String editora;
--- sistema/src/main/java/com/biblioteca/sistema/entities/Pessoas.java
+++ sistema/src/main/java/com/biblioteca/sistema/entities/Pessoas.java
@@ -2,11 +2,14 @@ package com.biblioteca.sistema.entities;
 
 import java.sql.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 @Table(name= "pessoas")
@@ -15,6 +18,10 @@ public class Pessoas {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "nome")
+    @NotBlank(message = "O nome é obrigatório!")
+    @Pattern(regexp = "[A-Za-z\\s]+", message = "O nome deve conter apenas letras!")
     private String nome;
     private String endereco;
     private String telefone;
